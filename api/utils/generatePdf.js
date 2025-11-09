aa839fc0824ce0b9ba1f69ba3a82e2d899c4da8f
webapps-repository <henry@hazcam.io>
2025-11-09 15:51:42 +1000

    Update generatePdf.js

--- api/utils/generatePdf.js
+++ api/utils/generatePdf.js
@@ -1,4 +1,6 @@
-// /api/utils/generatePdf.js
+// tabular /api/utils/generatePdf.js
+
+
 import PDFDocument from "pdfkit";
 import getStream from "get-stream";
 import { pdfmetrics } from "pdfkit/js/data/core";
