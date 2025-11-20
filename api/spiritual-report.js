29a569d25eb8ceea863a92407ccdc2189a8f51cc
webapps-repository <henry@hazcam.io>
2025-11-20 12:08:44 +1000

    Update spiritual-report.js

--- api/spiritual-report.js
+++ api/spiritual-report.js
@@ -5,12 +5,7 @@ import formidable from "formidable";
 import fs from "fs";
 
 // NEW IMPORT PATHS (lib instead of utils)
-import {
-  verifyRecaptcha,
-  sendEmailHTML,
-  validateUploadedFile
-} from "../lib/utils.js";
-
+import { verifyRecaptcha, sendEmailHTML, validateUploadedFile } from "../lib/utils.js";
 import { classifyQuestion } from "../lib/ai.js";
 import { analyzePalmImage } from "../lib/engines.js";
 import { generateInsights } from "../lib/insights.js";
