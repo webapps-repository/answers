007c077947bb872a6023e1a5b547c3918502a127
webapps-repository <henry@hazcam.io>
2025-11-24 16:21:53 +1000

    Update spiritual-report.js

--- api/spiritual-report.js
+++ api/spiritual-report.js
@@ -74,6 +74,8 @@ export default async function handler(req, res) {
   /* ----------------------------------------------------------
      Verify reCAPTCHA
   ---------------------------------------------------------- */
+  console.log("DEBUG recaptchaToken:", JSON.stringify(recaptchaToken));
+  console.log("DEBUG fields:", JSON.stringify(fields));
   const rec = await verifyRecaptcha(
     recaptchaToken,
     req.headers["x-forwarded-for"]
